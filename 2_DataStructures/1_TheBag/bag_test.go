395645d9334e0b61e730ec071bc176168d7215c0
Nathaniel J Cochran <nathanjcochran@gmail.com>
2019-09-30 14:26:46 -0400

    More tests

--- 2_DataStructures/1_TheBag/bag_test.go
+++ 2_DataStructures/1_TheBag/bag_test.go
@@ -38,6 +38,25 @@ func TestAdd_Multiple(t *testing.T) {
 	}
 }
 
+func TestAdd_Many(t *testing.T) {
+	bag := NewBag()
+
+	for i := 0; i < 100; i++ {
+		bag.Add(i)
+	}
+
+	for i := 0; i < 100; i++ {
+		if !bag.Contains(i) {
+			t.Errorf("Expected bag to contain %d after calling Add(%d)", i, i)
+			break
+		}
+	}
+
+	if bag.Size() != 100 {
+		t.Errorf("Expected bag to have a size of 100 after calling Add 100 times")
+	}
+}
+
 func TestAdd_Duplicates(t *testing.T) {
 	bag := NewBag()
 	bag.Add(1)
@@ -87,6 +106,29 @@ func TestRemove_Multiple(t *testing.T) {
 	}
 	if bag.Size() != 0 {
 		t.Errorf("Expected bag to have a size of 0 after calling Add(1), Add(2), Remove(1), and Remove(2)")
+	}
+}
+
+func TestRemove_Many(t *testing.T) {
+	bag := NewBag()
+
+	for i := 0; i < 100; i++ {
+		bag.Add(i)
+	}
+
+	for i := 0; i < 100; i++ {
+		bag.Remove(i)
+	}
+
+	for i := 0; i < 100; i++ {
+		if bag.Contains(i) {
+			t.Errorf("Expected bag to not contain %d after calling Add(%d) and Remove(%d)", i, i, i)
+			break
+		}
+	}
+
+	if bag.Size() != 0 {
+		t.Errorf("Expected bag to have a size of 100 after calling Add and Remove 100 times")
 	}
 }
 
