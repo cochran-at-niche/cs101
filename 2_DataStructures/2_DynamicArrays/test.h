a860d64f3729242393cf1149072cb039d5a99c4d
Nathaniel J Cochran <nathanjcochran@gmail.com>
2019-11-01 16:53:25 -0400

    Make proper test.h header file

---
+++ 2_DataStructures/2_DynamicArrays/test.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dynamic_array.h"
+
+#ifndef TEST_H
+#define TEST_H
+
+void test_new_dyn_arr();
+void test_dyn_arr_add();
+void test_dyn_arr_add_many();
+void test_dyn_arr_add_multiple();
+void test_dyn_arr_add_duplicates();
+void test_dyn_arr_remove();
+void test_dyn_arr_remove_multiple();
+void test_dyn_arr_remove_many();
+void test_dyn_arr_remove_non_existent();
+void test_dyn_arr_remove_duplicates();
+
+#endif
