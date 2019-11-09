08dac08ba87efa250ff29729b67b4620e94c95e7
daniel-salmon <dsalmon@niche.com>
2019-11-09 11:44:48 -0500

    Implement bag ADT to pass all tests

--- 2_DataStructures/1_TheBag/bag.go
+++ 2_DataStructures/1_TheBag/bag.go
@@ -4,23 +4,18 @@ package bag
 // for adding and removing items, checking if a given item exists in the bag,
 // and getting the number of items stored in the bag.
 type Bag struct {
-	// TODO: Add fields here for storing items. You can use any data structure
-	// you want (there is no "right" answer). The fields you add here will be
-	// accessible in the methods below. See: https://gobyexample.com/methods
+	Items []int
 }
 
 // NewBag is used to create an instance of a Bag.
 func NewBag() *Bag {
-	return &Bag{
-		// TODO: If necessary, initialize fields here.
-	}
+	return &Bag{}
 }
 
 // Add inserts an item into the bag. The bag should accept and store duplicate
 // items (i.e. it is not a set).
 func (b *Bag) Add(i int) {
-	// TODO: Implement the Add method.
-
+	b.Items = append(b.Items, i)
 	return
 }
 
@@ -28,7 +23,31 @@ func (b *Bag) Add(i int) {
 // items in the bag, it should only remove one of them. If the item does not
 // exist in the bag, it should silently do nothing.
 func (b *Bag) Remove(i int) {
-	// TODO: Implement the Remove method.
+	// First, find the index of the item we want to remove from the bag
+	idx := -1
+	for k, item := range b.Items {
+		if item == i {
+			idx = k
+			break
+		}
+	}
+
+	// If a match was found, remove that element from our slice of Items
+	// Otherwise, do nothing
+	if idx >= 0 {
+		b.remove_idx(idx)
+	}
+
+	return
+}
+
+func (b *Bag) remove_idx(idx int) {
+	// Swap the item at idx with the last item located at lastidx
+	lastidx := len(b.Items) - 1
+	b.Items[lastidx], b.Items[idx] = b.Items[idx], b.Items[lastidx]
+
+	// Truncate the bag to all but the last item
+	b.Items = b.Items[:lastidx]
 
 	return
 }
@@ -36,14 +55,15 @@ func (b *Bag) Remove(i int) {
 // Contains returns true if the given item exists in the bag, and false
 // otherwise.
 func (b *Bag) Contains(i int) bool {
-	// TODO: Implement the Contains method.
-
+	for _, item := range b.Items {
+		if item == i {
+			return true
+		}
+	}
 	return false
 }
 
 // Size returns the number of items in the bag.
 func (b *Bag) Size() int {
-	// TODO: Implement the size method.
-
-	return 0
+	return len(b.Items)
 }
