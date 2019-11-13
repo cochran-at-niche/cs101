5e60040c074622a5f55fbf0f1a6ffdda458f3621
Nathaniel J Cochran <nathanjcochran@gmail.com>
2019-11-13 11:44:14 -0500

    Better scratch space

--- 2_DataStructures/2_DynamicArrays/scratch/README.md
+++ 2_DataStructures/2_DynamicArrays/scratch/README.md
@@ -2,4 +2,4 @@
 
 This is just some scratch space I set up, in case you want to play around with
 the C programming language. All of the examples from the first lecture's slides
-are in main.c (commented out).
+are in main.c (uncommented the correct function call in main() to run).
--- 2_DataStructures/2_DynamicArrays/scratch/main.c
+++ 2_DataStructures/2_DynamicArrays/scratch/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+// Pointers example
+
+void pointers() {
     int b = 17;
     int* a = &b;
 
@@ -12,93 +14,104 @@ int main() {
     printf(" b: %d\n", b);
 }
 
-//int main() {
-//    printf("sizeof(int): %lu\n",
-//        sizeof(int)
-//    );
-//    printf("sizeof(int*): %lu\n",
-//        sizeof(int*)
-//    );
-//}
+// Sizeof example
 
+void sizes() {
+    printf("sizeof(int): %lu\n",
+        sizeof(int)
+    );
+    printf("sizeof(int*): %lu\n",
+        sizeof(int*)
+    );
+}
 
-//int* get_int_ptr() {
-//    int b = 17;
-//    return &b;
-//}
-//
-//int main() {
-//    int* a = get_int_ptr();
-//    printf("*a: %d\n", *a);
-//}
+// Segmentation fault example
 
-//int* get_int_ptr() {
-//    int *b = malloc(sizeof(int));
-//    *b = 17;
-//    return b;
-//}
-//
-//int main() {
-//    int* a = get_int_ptr();
-//    printf("*a: %d\n", *a);
-//    free(a);
-//}
+int* get_int_ptr1() {
+    int b = 17;
+    return &b;
+}
 
-//typedef struct{
-//    int i;
-//    int* j;
-//} my_struct;
-//
-//int main() {
-//    printf("sizeof(int[3]): %lu\n",
-//        sizeof(int[3])
-//    );
-//    printf("sizeof(my_struct): %lu\n",
-//        sizeof(my_struct)
-//    );
-//    printf("sizeof(my_struct*): %lu\n",
-//        sizeof(my_struct*)
-//    );
-//}
+void segmentation_fault() {
+    int* a = get_int_ptr1();
+    printf("*a: %d\n", *a);
+}
 
-//int main() {
-//    int j[2] = {1, 2};
-//    int k[3] = {1, 2, 3};
-//    j[2] = 4; // Out of bounds!
-//
-//    for (int i=0; i < 3; i++) {
-//        printf("k[%d]: %d\n", i, k[i]);
-//    }
-//}
+// Malloc and free example
 
-//int main() {
-//    int j[3];
-//    int* k = malloc(sizeof(int) * 3);
-//
-//    // Subscript iteration
-//    for (int i=0; i<3; i++) {
-//        j[i] = i;
-//        k[i] = i;
-//    }
-//
-//    // Pointer arithmetic iteration
-//    for (int* i=j; i < j+3; i++) {
-//        printf("%p: %d\n", i, *i);
-//    }
-//    printf("\n");
-//
-//    for (int* i=k; i < k+3; i++) {
-//        printf("%p: %d\n", i, *i);
-//    }
-//    printf("\n");
-//}
+int* get_int_ptr2() {
+    int *b = malloc(sizeof(int));
+    *b = 17;
+    return b;
+}
 
+void malloc_and_free() {
+    int* a = get_int_ptr2();
+    printf("*a: %d\n", *a);
+    free(a);
+}
 
+// Structs and arrays example
 
+typedef struct{
+    int i;
+    int* j;
+} my_struct;
 
+void structs_and_arrays() {
+    printf("sizeof(int[3]): %lu\n",
+        sizeof(int[3])
+    );
+    printf("sizeof(my_struct): %lu\n",
+        sizeof(my_struct)
+    );
+    printf("sizeof(my_struct*): %lu\n",
+        sizeof(my_struct*)
+    );
+}
 
+// Out of bounds example
 
+void out_of_bounds() {
+    int j[2] = {1, 2};
+    int k[3] = {1, 2, 3};
+    j[2] = 4; // Out of bounds!
 
+    for (int i=0; i < 3; i++) {
+        printf("k[%d]: %d\n", i, k[i]);
+    }
+}
 
+// Pointer arithmetic example
 
+void pointer_arithmetic() {
+    int j[3];
+    int* k = malloc(sizeof(int) * 3);
 
+    // Subscript iteration
+    for (int i=0; i<3; i++) {
+        j[i] = i;
+        k[i] = i;
+    }
+
+    // Pointer arithmetic iteration
+    for (int* i=j; i < j+3; i++) {
+        printf("%p: %d\n", i, *i);
+    }
+    printf("\n");
+
+    for (int* i=k; i < k+3; i++) {
+        printf("%p: %d\n", i, *i);
+    }
+    printf("\n");
+}
+
+int main() {
+    pointers();
+    //sizes();
+    //segmentation_fault();
+    //malloc_and_free();
+    //structs_and_arrays();
+    //out_of_bounds();
+    //pointer_arithmetic();
+}
