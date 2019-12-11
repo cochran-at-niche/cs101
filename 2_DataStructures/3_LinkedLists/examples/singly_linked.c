9e76f9d9752f2ecee85fd43e3a17ecf0608f9740
Nathaniel J Cochran <nathanjcochran@gmail.com>
2019-12-11 10:43:02 -0500

    Improve singly_linked.c example

--- 2_DataStructures/3_LinkedLists/examples/singly_linked.c
+++ 2_DataStructures/3_LinkedLists/examples/singly_linked.c
@@ -1,43 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct node {
+typedef struct node {
     int val;
     struct node* next;
-};
+} node;
 
-struct node* new_node(int val) {
-    struct node* new_node = (struct node*) malloc(sizeof(struct node));
+node* new_node(int val) {
+    node* new_node = (node*) malloc(sizeof(node));
     new_node->val = val;
     return new_node;
 }
 
-void traverse(struct node* n) {
-    for (struct node* cur = n; cur != NULL; cur = cur->next) {
+void traverse(node* head) {
+    for (node* cur = head; cur != NULL; cur = cur->next) {
         printf("%d\n", cur->val);
     }
 }
 
-struct node* insert_after(struct node* n, int val) {
+node* insert_after(node* n, int val) {
     struct  node* new = new_node(val);
     new->next = n->next;
     n->next = new;
     return new;
 }
 
-void remove_after(struct node* n) {
+void remove_after(node* n) {
     if (n->next == NULL) {
         return;
     }
 
-    struct node* tmp = n->next;
+    node* tmp = n->next;
     n->next = tmp->next;
     free(tmp);
 }
 
 int main() {
-    struct node* head = new_node(0);
-    struct node* n = head;
+    node* head = new_node(0);
+
+    node* n = head;
     for (int i=1; i<=10; i++) {
         n = insert_after(n, i);
     }
