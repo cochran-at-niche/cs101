c59fa331656c5004a1cf545a54347316b04455c1
Nathaniel J Cochran <nathanjcochran@gmail.com>
2020-01-06 16:11:56 -0500

    Better example, based on complete dynamic array implementation

--- 2_DataStructures/4_SearchingSorting/examples/search.c
+++ 2_DataStructures/4_SearchingSorting/examples/search.c
@@ -1,73 +1,37 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h> 
+#include "dynamic_array.h"
 
 const int SIZE = 100; 
 
-bool brute_force(int* arr, int size, int target) {
-    for (int i = 0; i<size; i++) {
-        if (arr[i] == target) {
-            return true;
-        }
-    }
-    return false;
-}
-
-bool binary_search(int* arr, int size, int target) {
-    int low=0;
-    int high=size;
-
-    while (low < high) {
-        int mid = (low + high) / 2;
-        if (arr[mid] < target) {
-            low = mid + 1;
-        } else if (arr[mid] > target) {
-            high = mid;
-        } else {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-int comp (const void * elem1, const void * elem2) 
-{
-    int f = *((int*)elem1);
-    int s = *((int*)elem2);
-    if (f > s) {
-        return  1;
-    } else if (f < s) {
-        return -1;
-    } else {
-        return 0;
-    }
-}
-
 int main() {
-    int arr[SIZE];
+    // Initialize a new dynamic array
+    dyn_arr* a = new_dyn_arr();
 
+    // Add a bunch of random numbers to it
     srand(time(0));
     for (int i = 0; i<SIZE; i++) {
-        arr[i] = rand() % SIZE;
+        dyn_arr_add(a, rand() % SIZE);
     }
 
-    qsort(arr, SIZE, sizeof(*arr), comp);
+    // Sort it
+    dyn_arr_sort(a);
 
+    // Print it
     printf("Array:");
-    for (int i = 0; i<SIZE; i++) {
-        printf(" %d", arr[i]);
-    }
-    printf("\n");
+    dyn_arr_print(a);
 
+    // Prompt user for a number to search for
     printf("Enter an integer to search for:\n");
     int target;
     scanf("%d", &target);
 
-    int j = brute_force(arr, SIZE, target);
+    // Brute force search
+    int j = dyn_arr_brute_force(a, target);
     printf("Brute force: %s\n", j ? "true" : "false");
 
-    int k = binary_search(arr, SIZE, target);
+    // Binary search
+    int k = dyn_arr_binary_search(a, target);
     printf("Binary search: %s\n", k ? "true" : "false");
 }
