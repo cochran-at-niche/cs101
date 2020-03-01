36d0dcfdc1e4d052fc38a2b2783d09c6843498ce
daniel-salmon <me@daniel-salmon.com>
2020-03-01 15:42:41 -0500

    Refactor everything because i learned some lessons

--- 2_DataStructures/5_BinaryTrees/binary_tree.c
+++ 2_DataStructures/5_BinaryTrees/binary_tree.c
@@ -7,18 +7,35 @@
 binary_tree* new_binary_tree() {
     // Allocate memory for a new binary tree
     binary_tree* t = (binary_tree*) malloc(sizeof(binary_tree));
+    t->root = NULL;
     t->size = 0;
     return t;
 }
 
-node* new_node() {
+node* new_node(int i) {
     node* n = (node*) malloc(sizeof(node));
-    n->val = 0;
+    n->left = NULL;
+    n->right = NULL;
+    n->val = i;
     return n;
 }
 
 void binary_tree_add(binary_tree *t, int i) {
-    // TODO
+    node_add(&t->root, i);
+    t->size++;
+}
+
+void node_add(node **root, int i) {
+    if (!(*root)) {
+        *root = new_node(i);
+    } else if (i < (*root)->val) {
+        node_add(&(*root)->left, i);
+    } else if (i > (*root)->val) {
+        node_add(&(*root)->right, i);
+    } else {
+        // The value i already exists in the tree
+        return;
+    }
 }
 
 void binary_tree_remove(binary_tree *t, int i) {
@@ -26,13 +43,27 @@ void binary_tree_remove(binary_tree *t, int i) {
 }
 
 bool binary_tree_contains(binary_tree *t, int i) {
-    // TODO
-    return false;
+    if (find_node(t->root, i)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+node* find_node(node *root, int i) {
+    if (!root) {
+        return NULL;
+    } else if (i == root->val) {
+        return root;
+    } else if (i < root->val) {
+        return find_node(root->left, i);
+    } else {
+        return find_node(root->right, i);
+    }
 }
 
 int binary_tree_size(binary_tree *t) {
-    // TODO
-    return 0;
+    return t->size;
 }
 
 void print_node(node *n) {
@@ -54,5 +85,15 @@ void binary_tree_print(binary_tree *t) {
 }
 
 void free_binary_tree(binary_tree *t) {
-    // TODO
+    free_node(t->root);
+    free(t);
+}
+
+void free_node(node *root) {
+    if (!root) {
+        return;
+    }
+    free_node(root->left);
+    free_node(root->right);
+    free(root);
 }
--- 2_DataStructures/5_BinaryTrees/binary_tree.h
+++ 2_DataStructures/5_BinaryTrees/binary_tree.h
@@ -19,8 +19,14 @@ typedef struct {
 // its fields.
 binary_tree* new_binary_tree() ;
 
+// new_node allocates memory for a new node and initializes its fields
+node* new_node(int i);
+
 // binary_tree_add inserts an item into the binary tree.
 void binary_tree_add(binary_tree *t, int i);
+
+// node_add inserts a new node into the tree of which root is a member
+void node_add(node **root, int i);
 
 // binary_tree_remove deletes an item from the binary tree.
 void binary_tree_remove(binary_tree *t, int i);
@@ -28,6 +34,10 @@ void binary_tree_remove(binary_tree *t, int i);
 // binary_tree_contains returns true if the given item exists in the binary
 // tree, and false otherwise.
 bool binary_tree_contains(binary_tree *t, int i);
+
+// find_node returns a pointer to the node in the tree that has the value i
+// if no such node exists, find_node returns NULL. find_node recursively traverses the tree
+node* find_node(node *root, int i);
 
 // binary_tree_size returns the number of items currently in the binary tree.
 int binary_tree_size(binary_tree *t);
@@ -38,5 +48,9 @@ void binary_tree_print(binary_tree *t);
 // free_binary_tree frees the memory allocated for the binary tree (both the
 // nodes of the tree, as well as the binary tree header struct)
 void free_binary_tree(binary_tree *t);
+
+// free_node frees the memory allocated for the node,
+// recursively freeing memory for all child nodes below it
+void free_node(node *root);
 
 #endif
